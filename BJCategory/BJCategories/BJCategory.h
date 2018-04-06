bed7fc4491d6f6b3ed18f1fa728258a690c9a028
genghongkai <1159538747@qq.com>
2018-04-06 20:31:58 +0800

    增加了找一个类(kvc)的key

--- BJCategory/BJCategories/BJCategory.h
+++ BJCategory/BJCategories/BJCategory.h
@@ -5,7 +5,7 @@
 //  Created by ghk on 2018/3/30.
 //  Copyright © 2018年 geng hongkai. All rights reserved.
 //  代码地址：https://github.com/genghongkai/BJCategory
-//  0.1.0
+//  0.1.1
 
 #ifndef BJCategory_h
 #define BJCategory_h
